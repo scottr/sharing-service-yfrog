e7f7d82a2283f405420bba0c576f8f4a53e4e260
Scott Raynel <scottraynel@gmail.com>
2010-03-25 22:55:16 +0000

    Add twitter account validation on account setup.

--- src/validate.c
+++ src/validate.c
@@ -1,5 +1,11 @@
 /*
- * This file is part of sharing-plugin-template
+ * validate.c
+ * Account validation functions for sharing-service-yfrog
+ *
+ * (C) 2010 Scott Raynel <scottraynel@gmail.com>
+ * This code is licensed under the GPLv2.
+ *
+ * This file was part of sharing-plugin-template
  *
  * Copyright (C) 2008-2009 Nokia Corporation. All rights reserved.
  *
@@ -11,6 +17,8 @@
 
 #include <stdio.h>
 #include <glib.h>
+#include <glib/gprintf.h>
+#include <string.h>
 #include <sharing-account.h>
 #include <sharing-http.h>
 #include <osso-log.h>
@@ -31,25 +39,45 @@
 SharingPluginInterfaceAccountValidateResult validate (SharingAccount* account,
     ConIcConnection* con, gboolean *cont, gboolean* dead_mans_switch)
 {
-    SharingPluginInterfaceAccountValidateResult ret =
-        SHARING_ACCOUNT_VALIDATE_SUCCESS;
+	SharingPluginInterfaceAccountValidateResult ret =
+		SHARING_ACCOUNT_VALIDATE_SUCCESS;
 
-    SharingHTTP * http = sharing_http_new ();
+	gchar *user = sharing_account_get_username(account);
+	gchar *password = sharing_account_get_password(account);
+	gchar buffer[1204];
 
-    /* Correct fields must be added to http request before sending */
+	/* Sanity check user/pass lengths */
+	if ((strlen(user) + strlen(password)) > 1024)
+		return SHARING_ACCOUNT_VALIDATE_NOT_STARTED;
 
-    SharingHTTPRunResponse res;
-    res = sharing_http_run (http, "http://example.com/post");
-    if (res == SHARING_HTTP_RUNRES_SUCCESS) {
-      ULOG_DEBUG_L ("Got response (%d): %s\n", sharing_http_get_res_code (http),
-		    sharing_http_get_res_body (http, NULL));
-    } else {
-      ULOG_ERR_L ("Couldn't get stuff from service\n");
-      ret = SHARING_ACCOUNT_VALIDATE_FAILED;
-    }
-    sharing_http_unref (http); 
-    
+	/* Create b64 encoded user/pass into buffer */
+	snprintf(buffer, sizeof(buffer), "%s:%s", user, password);
+	gchar *encoded = g_base64_encode((guchar *)buffer, (gsize)strlen(buffer));
+	if (!encoded)
+		return SHARING_ACCOUNT_VALIDATE_ERROR_UNKNOWN;
+	snprintf(buffer, sizeof(buffer), "Basic %s", encoded);
 
-    return ret;
+	g_free(encoded);
+
+	SharingHTTP * http = sharing_http_new ();
+
+	sharing_http_add_req_header(http, "Authorization", buffer);
+
+	/* Reduce the amount of data we need to get back */
+	sharing_http_add_req_header(http, "Accept-Encoding", "gzip,deflate");
+
+	SharingHTTPRunResponse res;
+	res = sharing_http_run (http, "http://api.twitter.com/1/account/verify_credentials.xml");
+
+	if (res == SHARING_HTTP_RUNRES_SUCCESS) {
+		if (sharing_http_get_res_code(http) != 200)
+			ret = SHARING_ACCOUNT_VALIDATE_FAILED;
+	} else {
+		ret = SHARING_ACCOUNT_VALIDATE_FAILED;
+	}
+
+	sharing_http_unref (http);
+
+	return ret;
 }
 
